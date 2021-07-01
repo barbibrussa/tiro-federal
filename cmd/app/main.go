13cb68b20c1f857e99728a7976797ed8c44e7cb3
barbibrussa <barbibrussa@gmail.com>
2021-07-01 19:51:43 -0300

    Add database initialization and migrations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/barbibrussa/tiro-federal/pkg/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+)
 
+func main() {
+	db, err := gorm.Open(sqlite.Open("members.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error while connecting to the database: ", err)
+	}
+
+	err = db.AutoMigrate(&models.Member{})
+	if err != nil {
+		log.Fatal("Error while auto-migrating models: ", err)
+	}
 }
